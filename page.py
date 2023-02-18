d7b0094eba61a893b2334b0b0f64b2df46b57c57
dfasaAZ <49995905+dfasaAZ@users.noreply.github.com>
2023-02-18 16:44:32 +0300

    Шаблон под обработку excel

--- page.py
+++ page.py
@@ -6,12 +6,12 @@ from dash import dcc, html
 from dash.dependencies import Input, Output, State
 
 
-
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-with open("data.yaml", "r", encoding='utf-8') as f:#Читаем файл и записываем все данные оттуда
+# Читаем файл и записываем все данные оттуда
+with open("data.yaml", "r", encoding='utf-8') as f:
     data = yaml.safe_load(f)
-    
+
 app = dash.Dash(__name__)
 
 colors = {
@@ -36,38 +36,47 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                                                                        value="Чеченская Республика",
                                                                        options=[{'label': x, 'value': x} for x in
                                                                                 data]),
-                                                          html.Div(id="output-div", children=[]), ]
+                                                          html.Div(id="output-dobro-div", children=[]), html.Div(id="output-excel-div", children=[])]
              ),
 
 ])
 
 
-@app.callback(Output(component_id="output-div", component_property="children"),
+@app.callback(Output(component_id="output-dobro-div", component_property="children"),
               Input(component_id="region", component_property="value"),
-              )#Берет параметр value у компонентов с id region и передает результат последующей функции в компонент с id output-div
-def buildGraphs(chosenRegion):
+              )  # Берет параметр value у компонентов с id region и передает результат последующей функции в компонент с id output-div
+def buildDobroGraphs(chosenRegion):
     """
     Входной параметр передается через value у объекта с  id region
     Функция возвращает разметку страницы, которая содержит подгружаемые динамически данные 
 
     """
     liList = []
-    p_comp = data[chosenRegion]#Берет все строки с регионом
+    # Берет все строки с регионом
+    p_comp = data[chosenRegion]
     for x in p_comp:
         print(str(x))
         liList.append(html.Li(children=html.A(str(x))))
     p_data = [p_comp[0]["Организаторы"],
               p_comp[1]["Волонтёры"]]
     fig_comp = px.pie(values=p_data, names=[
-                      'организаторы', 'волонтеры'])# График пирог
+                      'организаторы', 'волонтеры'])  # График пирог
     bar_comp = px.bar(x=['организаторы',
-                      'волонтеры'], y=p_data)# График столбцы
+                      'волонтеры'], y=p_data)  # График столбцы
     return html.Div(children=[
         dcc.Graph(figure=fig_comp)],
         style={
             'display': 'inline-block'}), html.Div(
         children=[dcc.Graph(figure=bar_comp)], style={'display': 'inline-block'}), html.Ul(
         style={'color': colors['text']}, children=html.Li(liList))
+
+
+@app.callback(Output(component_id="output-excel-div", component_property="children"),
+              Input(component_id="region", component_property="value"),
+              )
+def BuildForm1Graphs(chosenRegion):
+    # TODO: Обработать данные из csv файла
+    return html.A(f"Здесь описать вывод данных по файлу excel для {chosenRegion}", style={'color': colors['text']},)
 
 
 if __name__ == '__main__':
